e002e86aeb62d8e673490a52fffae63a5b30c06f
jerrywang761208 <c1495616@gmail.com>
2018-09-29 21:56:57 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use:{
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+}
